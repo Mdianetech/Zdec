81eecf9acc35824f94713456165689d1296cf887
Mdianetech <mamoudou6976diane@gmail.com>
2025-06-02 15:47:23 +0200

    Update image styling in AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -181,7 +181,7 @@ const AboutPage = () => {
                     <img 
                       src={member.image}
                       alt={member.name}
-                      className="w-full h-full object-cover"
+                      className="w-full h-full object-cover object-top"
                     />
                   )}
                 </div>
